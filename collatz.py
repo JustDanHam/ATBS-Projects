e78e9a80e1d7c321b6faa6b934aee7e1f86843e1
JustDanHam <134406405+JustDanHam@users.noreply.github.com>
2023-05-23 16:43:03 +0100

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,18 @@
+#!
+
+def collatz(number):
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    elif number % 2 == 1:
+        print(3 * number + 1)
+        return 3 * number + 1
+
+num = input('Enter a number: ')
+
+try:
+    while num != 1:
+        num = collatz(int(num))
+
+except ValueError:
+    print('Please enter an integer')
